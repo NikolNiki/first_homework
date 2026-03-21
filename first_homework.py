23795a5076a52794e814f8c7949d39446fb743d2
NikiNikol <nikinikol1610@icloud.com>
2026-03-21 16:30:48 +0100

    first homework

---
+++ first_homework.py
@@ -0,0 +1,19 @@
+favorite_city = "Київ"
+city_association = "\U0001F330"
+print(f"Моє улюблене місто: {favorite_city}")
+print(f"Асоціація: {city_association}")
+umbrella_symbol = "☂"
+print(f"Порядковій номер символу `{umbrella_symbol}`: {ord(umbrella_symbol)}")
+pineapple_emoji = "\U0001F34D"
+print(f"Символ U+1F34D: {pineapple_emoji}")
+eneida_fragment ="""
+Еней був парубок моторний
+І хлопець хоть куди козак,
+На лихо удавсь гордий,
+І завзятійший од всіх
+"""
+print("Уривок з `Енеїди`")
+print(eneida_fragment)
+
+
+
